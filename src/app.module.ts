a2b6f78aeeae3eb3f330f2019f5b38b264e2467d
tranxuan1096 <tranxuan.1096@gmail.com>
2025-01-07 14:28:32 +0700

    chore: delete unnecessery modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-import { StoresModule } from './modules/stores/stores.module';
-import { CuisinesModule } from './modules/cuisines/cuisines.module';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './modules/auth/auth.module';
-import { UsersModule } from './modules/users/users.module';
-import { ProductsModule } from './modules/products/products.module';
-import { ProductCategoriesModule } from './modules/product-categories/product-categories.module';
 import { BillsModule } from './modules/bills/bills.module';
 import { CustomersModule } from './modules/customers/customers.module';
-import { InsightModule } from './modules/insight/insight.module';
+import { UsersModule } from './modules/users/users.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -17,15 +12,10 @@ import { InsightModule } from './modules/insight/insight.module';
       envFilePath: '.env',
     }),
     MongooseModule.forRoot(process.env.CONNECTIONSTRING),
-    ProductsModule,
-    StoresModule,
-    CuisinesModule,
     AuthModule,
     UsersModule,
-    ProductCategoriesModule,
     BillsModule,
     CustomersModule,
-    InsightModule,
   ],
   // providers: [
   //   {
